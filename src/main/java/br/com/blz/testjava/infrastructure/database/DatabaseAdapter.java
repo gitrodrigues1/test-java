e61c252ef46d8e97826b7f383c07a7fb2b45b325
gitrodrigues1 <rodrigues.ramos@gmail.com>
2024-07-11 11:41:15 -0300

    add database adapter

---
+++ src/main/java/br/com/blz/testjava/infrastructure/database/DatabaseAdapter.java
@@ -0,0 +1,55 @@
+package br.com.blz.testjava.infrastructure.database;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Repository;
+
+import br.com.blz.testjava.core.model.Product;
+
+@Repository
+public class DatabaseAdapter {
+
+    List<Product> products = new ArrayList<>();
+
+    public List<Product> getProducts() {
+        return products;
+    }
+
+    public void addProduct(Product product) {
+        this.products.add(product);
+    }
+
+    public Product editProduct(long sku, Product product) {
+        
+        for (Product p : products) {
+            if(p.getSku() == sku) {
+                p.setSku(product.getSku());
+                p.setName(product.getName());
+                p.setInventory(product.getInventory());
+                return p;
+            }
+        }
+
+        return null;
+    }
+
+    public Product getProduct(long sku) {
+        for (Product p : products) {
+            if(p.getSku() == sku) {
+                return p;
+            }
+        }
+        return null;
+    }
+
+    public void removeProduct(long sku) {
+        for (Product p : products) {
+            if(p.getSku() == sku) {
+                this.products.remove(p);
+                break;
+            }
+        }
+    }
+    
+}
